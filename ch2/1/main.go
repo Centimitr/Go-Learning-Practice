1ac87894d19834ef825afa13591aaa767eeacbcc
Centimitr <Centimitr@163.com>
2016-02-08 19:39:59 +0800

    add ch2 2.1

---
+++ ch2/1/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"tempconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+		f := tempconv.Fahrenheit(t)
+		k := tempconv.Kelvin(t)
+		fmt.Printf("%s = %s, %s = %s\n",
+			f, tempconv.KToC(k), c, tempconv.CToK(c))
+	}
+}
