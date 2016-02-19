bb98793a33c5d584bb538e4d30bac3dd8db5f539
Centimitr <Centimitr@163.com>
2016-02-19 20:52:26 +0800

    add avg

---
+++ ot/avg/avg.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// var avg = []int{}
+	sum := 0
+	file := os.Args[1]
+	content, _ := ioutil.ReadFile(file)
+	scores := strings.Split(string(content), ",")
+	for _, scoreStr := range scores {
+		score, _ := strconv.Atoi(scoreStr)
+		sum += score
+	}
+	fmt.Println(float64(sum) / float64(len(scores)))
+}
